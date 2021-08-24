05702dde4e688b1a95106fda11ece3c0d440ee3a
Guillermo Aragón <guillearar97@gmail.com>
2021-08-24 17:12:43 -0500

    Primera version con swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 // import { getAllUsers, addOneUser, updateOneUser, deleteOneUser } from './Users';
 import {getEpisode, getRecentEmitted} from './Animes';
+import swagger from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+const docs = YAML.load(path.join(__dirname, '/../shared/swagger.yaml'))
 
 
 // User-route
@@ -19,5 +23,6 @@ animeRouter.get('/episode', getEpisode);
 // Export the base-router
 const baseRouter = Router();
 baseRouter.use('/anime', animeRouter);
+baseRouter.use('/api-docs', swagger.serve, swagger.setup(docs));
 // baseRouter.use('/users', userRouter);
 export default baseRouter;
