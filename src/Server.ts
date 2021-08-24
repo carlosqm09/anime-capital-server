a9eb921292607d441980d2a11b035f1263d6cc7b
Guillermo Aragón <guillearar97@gmail.com>
2021-08-24 17:32:35 -0500

    Cors para localhost

--- src/Server.ts
+++ src/Server.ts
@@ -34,6 +34,15 @@ if (process.env.NODE_ENV === 'production') {
     app.use(helmet());
 }
 
+//CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 // Add APIs
 app.use('/api', BaseRouter);
 
