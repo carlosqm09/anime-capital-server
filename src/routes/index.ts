36bb7d71c5e46ab902aa6c5cce9c2fe6f9761a2a
Guillermo Aragón <guillearar97@gmail.com>
2021-08-26 18:57:51 -0500

    Cors en la api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -24,6 +24,15 @@ animeRouter.get('/episode', getEpisode);
 
 // Export the base-router
 const baseRouter = Router();
+
+baseRouter.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 baseRouter.use('/anime', animeRouter);
 baseRouter.get('/', redir);
 baseRouter.use('/api-docs', swagger.serve, swagger.setup(docs));
