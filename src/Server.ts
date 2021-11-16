9bb116d10674ebad2b44126c7d027a65882cd716
Carlos Quijada Medina <52507254+carlosqm09@users.noreply.github.com>
2021-11-16 16:19:35 -0600

    Update Server.ts

--- src/Server.ts
+++ src/Server.ts
@@ -39,13 +39,13 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 //CORS
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-//     next();
-// });
+app.use((req, res, next) => {
+     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+     next();
+ });
 
 // Add APIs
 app.use('/api', BaseRouter);
